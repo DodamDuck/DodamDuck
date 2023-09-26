8fe97181451d53d990852d39b8bad6917e75d827
yunyoung0531 <yunyoung0531@naver.com>
2023-09-26 17:35:41 +0900

    [FE] chore: 프로젝트 초기 세팅

---
+++ dodamduck_fe/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        프로젝트 세팅
+      </div>
+    </>
+  );
+}
+
+export default App;
