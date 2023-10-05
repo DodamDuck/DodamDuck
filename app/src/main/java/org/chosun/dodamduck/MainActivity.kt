e3ba0179d473550990e35ded661234360d7d2924
casio2978 <casio2978@naver.com>
2023-10-05 17:25:19 +0900

    [AOS] feat: 하단 네비게이션 연동

--- app/src/main/java/org/chosun/dodamduck/MainActivity.kt
+++ app/src/main/java/org/chosun/dodamduck/MainActivity.kt
@@ -3,13 +3,15 @@ package org.chosun.dodamduck
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import org.chosun.dodamduck.ui.navigation.BottomNavigation
+import org.chosun.dodamduck.ui.navigation.daoDamDuckNavigationGraph
+import androidx.navigation.compose.rememberNavController
 import org.chosun.dodamduck.ui.theme.DodamDuckTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +19,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DodamDuckTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreenView()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreenView() {
+    val navController = rememberNavController()
+    Scaffold (
+        bottomBar = { BottomNavigation(navController = navController) }
+    ) {
+        Box(Modifier.padding(it)) {
+            daoDamDuckNavigationGraph(navController = navController)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     DodamDuckTheme {
-        Greeting("Android")
+        MainScreenView()
     }
 }
