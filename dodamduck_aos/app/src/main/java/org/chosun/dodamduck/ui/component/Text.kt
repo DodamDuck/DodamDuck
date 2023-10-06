3e9e6aa685b9b9edb055a270d81bbc32c365d035
sey2 <casio2978@naver.com>
2023-10-07 04:06:14 +0900

    [AOS] design: 텍스트 Component 구현

--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/component/Text.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/component/Text.kt
@@ -4,24 +4,73 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import org.chosun.dodamduck.R
 import org.chosun.dodamduck.ui.theme.Brown
 import org.chosun.dodamduck.ui.theme.Krona
 
 @Composable
 fun WelcomeText() {
     Text(
-        text = "Welcome",
+        text = stringResource(R.string.welcome),
         style = MaterialTheme.typography.titleMedium,
         color = Brown,
         fontFamily = Krona,
-        fontSize = 45.sp,
-        modifier = Modifier
+        fontSize = 45.sp
     )
 }
 @Preview(showBackground = true)
 @Composable
 fun PreviewWelcomeText() {
     WelcomeText()
+}
+
+@Composable
+fun DodamDuckTextH1(
+    modifier: Modifier = Modifier,
+    text: String,
+    color: Color = Color.Black
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        color = color,
+        fontFamily = Krona,
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun DodamDuckTextH2(
+    modifier: Modifier = Modifier,
+    text: String,
+    color: Color = Color.Black
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        color = color,
+        fontFamily = Krona,
+        fontSize = 16.sp
+    )
+}
+
+@Composable
+fun DodamDuckTextH3(
+    modifier: Modifier = Modifier,
+    text: String,
+    color: Color = Color.Black
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        color = color,
+        fontFamily = Krona,
+        fontSize = 14.sp
+    )
 }
