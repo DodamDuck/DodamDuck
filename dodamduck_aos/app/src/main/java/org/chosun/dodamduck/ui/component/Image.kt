74d312296c16afacedf67df8cf77db75b21cd226
casio2978 <casio2978@naver.com>
2023-10-08 19:29:30 +0900

    [AOS] design: GoogleIcon, VisibleIcon Component 추가

--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/component/Image.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/component/Image.kt
@@ -23,3 +23,31 @@ fun DodamDuckIcon(
             .size(size.dp)
     )
 }
+
+@Composable
+fun GoogleIcon(
+    modifier: Modifier = Modifier,
+    size: Int = 30
+) {
+    Image(
+        painter = painterResource(id = R.drawable.ic_google_30),
+        contentDescription = stringResource(R.string.google_icon),
+        contentScale = ContentScale.Fit,
+        modifier = modifier
+            .size(size.dp)
+    )
+}
+
+@Composable
+fun VisibleIcon(
+    modifier: Modifier = Modifier,
+    size: Int = 20
+) {
+    Image(
+        painter = painterResource(id = R.drawable.ic_visible),
+        contentDescription = stringResource(R.string.visible_icon),
+        contentScale = ContentScale.Fit,
+        modifier = modifier
+            .size(size.dp)
+    )
+}
