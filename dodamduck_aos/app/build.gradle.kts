ae5b3c5cea175a2714793b65242d7cbe49c3ffb4
casio2978 <casio2978@naver.com>
2023-10-29 22:01:42 +0900

    [AOS] chore: hilt 설정

--- dodamduck_aos/app/build.gradle.kts
+++ dodamduck_aos/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -61,6 +63,10 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    // hilt
+    implementation("com.google.dagger:hilt-android:2.44")
+    
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
