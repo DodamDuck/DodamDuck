a90385351bd90a12f2b89cbee549a08845364f8a
sey2 <casio2978@naver.com>
2023-10-31 02:50:38 +0900

    [AOS] design: 홈 화면 버튼 추가 (#55)
    * [AOS] design: 홈 화면 글쓰기 버튼 추가
    
    * [AOS] chore: 리소스 파일 추가

--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/Home.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/Home.kt
@@ -1,5 +1,6 @@
 package org.chosun.dodamduck.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,16 +8,24 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import org.chosun.dodamduck.R
 import org.chosun.dodamduck.ui.component.DodamDuckTextH2
 import org.chosun.dodamduck.ui.component.ExchangeItemList
 import org.chosun.dodamduck.ui.theme.Brown
@@ -33,14 +42,29 @@ fun HomeScreen() {
             Header()
             ExchangeItemList(modifier = Modifier.padding(top = 24.dp))
         }
+
+        OutlinedButton(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .height(60.dp)
+                .padding(end = 8.dp, bottom = 8.dp),
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.outlinedButtonColors(contentColor = Brown),
+            border = BorderStroke(width = 1.dp, color = Brown)
+        ) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Add Icon", tint = Brown)
+            Spacer(Modifier.padding(horizontal = 3.dp))
+            Text(stringResource(R.string.writing), color = Brown)
+        }
     }
 }
 
 @Composable
 fun Header() {
-    Row(modifier = Modifier
-        .fillMaxWidth()
-        .padding(start = 10.dp, end = 10.dp, top = 12.dp)
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 10.dp, end = 10.dp, top = 12.dp)
     ) {
         DodamDuckTextH2(text = "빛가람동", color = Brown)
         Icon(imageVector = Icons.Default.KeyboardArrowDown, contentDescription = "Arrow Icon")
