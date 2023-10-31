123b519d86c21cbbe5b327f6bb2255c412bc60f9
casio2978 <casio2978@naver.com>
2023-10-31 23:58:11 +0900

    [AOS] design: GrayRoundedBox 컴포넌트 추가

--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/component/Box.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/component/Box.kt
@@ -1,12 +1,18 @@
+package org.chosun.dodamduck.ui.component
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import org.chosun.dodamduck.ui.theme.Secondary
 
 @Composable
@@ -21,6 +27,21 @@ fun BottomRoundedBox(
             .fillMaxWidth()
             .clip(RoundedCornerShape(0.dp, 0.dp, endRound.dp, startRound.dp))
             .background(Secondary)
+    ) {
+        content()
+    }
+}
+
+@Composable
+fun GrayRoundedBox(
+    modifier: Modifier = Modifier,
+    size: Dp = 64.dp,
+    content: @Composable () -> Unit = {}
+) {
+    Box(
+        modifier = modifier
+            .border(1.dp, Color.Gray, RoundedCornerShape(8.dp))
+            .size(size),
     ) {
         content()
     }
