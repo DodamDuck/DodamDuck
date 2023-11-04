4b89e1d7e1baa52efcd39bb59b44f3f9e8cc68fa
casio2978 <casio2978@naver.com>
2023-11-04 23:19:43 +0900

    [AOS] design: 버튼 컴포넌트 추가

--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/component/Button.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/component/Button.kt
@@ -1,7 +1,9 @@
 package org.chosun.dodamduck.ui.component
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -11,11 +13,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.chosun.dodamduck.ui.theme.Brown
+import org.chosun.dodamduck.ui.theme.Gray5
 
 @Composable
 fun PrimaryButton(
@@ -23,15 +27,20 @@ fun PrimaryButton(
     elevation: Dp = 12.dp,
     startPadding: Dp = 32.dp,
     endPadding: Dp = 32.dp,
+    topPadding: Dp = 0.dp,
     shape: Shape = RoundedCornerShape(15.dp),
     modifier: Modifier = Modifier,
     text: String = "",
     onClick: () -> Unit = {}
 ) {
-    Button (
+    Button(
         modifier = modifier
             .height(height)
-            .padding(start = startPadding, end = endPadding)
+            .padding(
+                start = startPadding,
+                end = endPadding,
+                top = topPadding
+            )
             .shadow(elevation = elevation, shape = RoundedCornerShape(12.dp)),
         shape = shape,
         colors = ButtonDefaults.buttonColors(
@@ -40,8 +49,10 @@ fun PrimaryButton(
         ),
         onClick = onClick
     ) {
-        Text(text = text,
-             fontSize = 18.sp)
+        Text(
+            text = text,
+            fontSize = 18.sp
+        )
     }
 }
 
@@ -49,4 +60,44 @@ fun PrimaryButton(
 @Composable
 fun PreviewPrimaryButton() {
     PrimaryButton()
+}
+
+@Composable
+fun DodamDuckRadioButton(
+    modifier: Modifier,
+    text: String,
+    selected: Boolean,
+    onSelect: () -> Unit
+) {
+    Button(
+        modifier = modifier
+            .width(90.dp)
+            .height(40.dp),
+        onClick = onSelect,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = if (selected) Brown else Color.White,
+            contentColor = if (selected) Color.White else Color.Gray
+        ),
+        shape = RoundedCornerShape(50),
+        border = if (!selected) BorderStroke(1.dp, Color.Gray) else null,
+    ) {
+        Text(text = text, fontSize = 11.sp)
+    }
+}
+
+@Composable
+fun GrayButton(modifier: Modifier = Modifier, text: String) {
+    Button(
+        modifier = modifier
+            .width(120.dp)
+            .height(40.dp),
+        onClick = { },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Gray5,
+            contentColor = Color.Black
+        ),
+        shape = RoundedCornerShape(8)
+    ) {
+        Text(text = text, fontSize = 11.sp, fontWeight = FontWeight.SemiBold)
+    }
 }
