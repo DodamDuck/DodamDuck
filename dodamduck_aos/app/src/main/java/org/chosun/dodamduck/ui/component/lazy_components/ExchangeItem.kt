30b431b758307de86c963e74e878f165faaffc42
casio2978 <casio2978@naver.com>
2023-11-05 20:21:20 +0900

    [AOS] chore: 패키지 변경

--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/component/lazy_components/ExchangeItem.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/component/lazy_components/ExchangeItem.kt
@@ -1,4 +1,4 @@
-package org.chosun.dodamduck.ui.component
+package org.chosun.dodamduck.ui.component.lazy_components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
@@ -26,6 +26,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.chosun.dodamduck.R
+import org.chosun.dodamduck.ui.component.DodamDuckTextH3
 
 @Composable
 @Preview(showBackground = true)
