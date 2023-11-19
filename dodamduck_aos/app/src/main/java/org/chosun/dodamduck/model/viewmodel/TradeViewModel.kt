776e42a43691ee7c7c64639e43835f21ae4e4fb6
casio2978 <casio2978@naver.com>
2023-11-20 01:31:54 +0900

    [AOS] feat: 거래/나눔 글 작성 API 연동

--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/model/viewmodel/TradeViewModel.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/model/viewmodel/TradeViewModel.kt
@@ -1,12 +1,13 @@
 package org.chosun.dodamduck.model.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import org.chosun.dodamduck.model.dto.Trade
 import org.chosun.dodamduck.model.repository.TradeRepository
 import javax.inject.Inject
@@ -14,7 +15,7 @@ import javax.inject.Inject
 @HiltViewModel
 class TradeViewModel @Inject constructor(
     private val repository: TradeRepository
-): ViewModel() {
+) : ViewModel() {
 
     private val _tradeLists = MutableStateFlow<List<Trade>?>(null)
     val tradeLists: StateFlow<List<Trade>?> = _tradeLists
@@ -22,6 +23,19 @@ class TradeViewModel @Inject constructor(
     fun getTradeLists() {
         viewModelScope.launch {
             _tradeLists.value = repository.fetchTradeList()
+        }
+    }
+
+    fun uploadTrade(
+        userId: RequestBody,
+        categoryId: RequestBody,
+        title: RequestBody,
+        content: RequestBody,
+        location: RequestBody,
+        image: MultipartBody.Part
+    ) {
+        viewModelScope.launch {
+            repository.uploadTrade(userId, categoryId, title, content, location, image)
         }
     }
 }
--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/TradeWrite.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/TradeWrite.kt
@@ -1,5 +1,9 @@
 package org.chosun.dodamduck.ui
 
+import android.content.Context
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,7 +14,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
@@ -20,20 +23,26 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
 import org.chosun.dodamduck.R
+import org.chosun.dodamduck.model.viewmodel.TradeViewModel
 import org.chosun.dodamduck.ui.component.DodamDuckRadioButton
 import org.chosun.dodamduck.ui.component.DodamDuckText
 import org.chosun.dodamduck.ui.component.DodamDuckTextH2
@@ -44,9 +53,26 @@ import org.chosun.dodamduck.ui.component.PrimaryButton
 import org.chosun.dodamduck.ui.component.lazy_components.PhotoCountBox
 import org.chosun.dodamduck.ui.component.lazy_components.PhotoSelectionList
 import org.chosun.dodamduck.ui.theme.DodamDuckTheme
+import org.chosun.dodamduck.utils.Utils.uriToMultipartBody
 
 @Composable
-fun TradeWriteScreen(navController: NavHostController) {
+fun TradeWriteScreen(
+    navController: NavHostController,
+    tradeViewModel: TradeViewModel = hiltViewModel()
+) {
+    var imageList by remember { mutableStateOf<List<Uri>>(listOf()) }
+    val galleryLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        imageList = ((imageList + uri) ?: throw NullPointerException()) as List<Uri>
+    }
+
+    val context = LocalContext.current
+    var title by remember { mutableStateOf("") }
+    var detailDescription by remember { mutableStateOf("") }
+    var tradeLocation by remember { mutableStateOf("") }
+    var transactionType by remember { mutableIntStateOf(0) }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -54,25 +80,34 @@ fun TradeWriteScreen(navController: NavHostController) {
     ) {
         Column {
             TradeWriteScreenHeader(navController)
-            PhotoSelectionList(modifier = Modifier.padding(start = 12.dp, top = 23.dp))
+            PhotoSelectionList(
+                modifier = Modifier.padding(start = 12.dp, top = 23.dp),
+                onImageClick = { galleryLauncher.launch("image/*") },
+                imageList,
+            )
             TradeWriteInputText(
                 modifier = Modifier.padding(start = 12.dp, end = 12.dp, top = 8.dp),
                 titleText = stringResource(id = R.string.title),
-                text = stringResource(id = R.string.title)
+                text = stringResource(id = R.string.title),
+                value = title,
+                onValueChange = { title = it }
             )
             TradeTransactionType(
                 modifier = Modifier.padding(
                     start = 12.dp,
                     end = 12.dp,
                     top = 17.dp
-                )
+                ),
+                onValueChange = { transactionType = it}
             )
             TradeWriteInputText(
                 modifier = Modifier
                     .padding(start = 12.dp, end = 12.dp, top = 8.dp)
                     .height(130.dp),
                 titleText = stringResource(id = R.string.detail_description),
-                text = stringResource(id = R.string.detail_description_content)
+                text = stringResource(id = R.string.detail_description_content),
+                value = detailDescription,
+                onValueChange = { detailDescription = it }
             )
             GrayButton(
                 modifier = Modifier.padding(start = 12.dp, top = 17.dp),
@@ -81,13 +116,23 @@ fun TradeWriteScreen(navController: NavHostController) {
             TradeWriteInputText(
                 modifier = Modifier.padding(start = 12.dp, end = 12.dp, top = 8.dp),
                 titleText = stringResource(R.string.desired_trading_location),
-                text = stringResource(R.string.dummy_item_location)
+                text = stringResource(R.string.dummy_item_location),
+                value = tradeLocation,
+                onValueChange = { tradeLocation = it+1 }
             )
             PrimaryButton(
                 modifier = Modifier.fillMaxWidth(),
-                startPadding = 12.dp, endPadding = 12.dp, topPadding = 24.dp,
-                elevation = 0.dp, text = stringResource(R.string.post_completed), shape = RoundedCornerShape(6.dp),
-                height = 80.dp
+                startPadding = 12.dp,
+                endPadding = 12.dp,
+                topPadding = 24.dp,
+                elevation = 0.dp,
+                text = stringResource(R.string.post_completed),
+                shape = RoundedCornerShape(6.dp),
+                height = 80.dp,
+                onClick = {
+                    handleTradeUpload(tradeViewModel, context, imageList, "seyeong2", transactionType.toString(), title, detailDescription, tradeLocation)
+                    navController.popBackStack()
+                }
             )
         }
     }
@@ -100,7 +145,10 @@ fun TradeWriteScreenHeader(navController: NavHostController) {
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = CenterVertically
     ) {
-        IconButton(onClick = {navController.popBackStack()}, modifier = Modifier.wrapContentSize()) {
+        IconButton(
+            onClick = { navController.popBackStack() },
+            modifier = Modifier.wrapContentSize()
+        ) {
             Icon(
                 imageVector = Icons.Default.Close,
                 contentDescription = "Close Icon"
@@ -124,7 +172,13 @@ fun TradeWriteScreenHeader(navController: NavHostController) {
 }
 
 @Composable
-fun TradeWriteInputText(modifier: Modifier = Modifier, titleText: String, text: String) {
+fun TradeWriteInputText(
+    modifier: Modifier = Modifier,
+    titleText: String,
+    text: String = "",
+    onValueChange: (String) -> Unit,
+    value: String = ""
+) {
     DodamDuckText(
         modifier = Modifier.padding(start = 12.dp, top = 17.dp),
         text = titleText,
@@ -136,17 +190,18 @@ fun TradeWriteInputText(modifier: Modifier = Modifier, titleText: String, text: 
             .fillMaxWidth(),
         label = text,
         borderColor = Color.Gray,
-        borderWidth = 1
+        borderWidth = 1,
+        onValueChange = onValueChange,
+        value = value
     )
 }
 
 @Composable
-fun TradeTransactionType(modifier: Modifier = Modifier) {
+fun TradeTransactionType(modifier: Modifier = Modifier, onValueChange: (Int) -> Unit) {
     var selectedOption by remember { mutableStateOf("exchange") }
     val options = listOf(
-        "exchange" to stringResource(R.string.exchange), "donation" to stringResource(
-            R.string.donation
-        )
+        "exchange" to stringResource(R.string.exchange),
+        "donation" to stringResource(R.string.donation)
     )
 
     Column(modifier = modifier) {
@@ -157,12 +212,15 @@ fun TradeTransactionType(modifier: Modifier = Modifier) {
         )
 
         Row(verticalAlignment = CenterVertically) {
-            options.forEach { (option, label) ->
+            options.forEachIndexed { index, (option, label) ->
                 DodamDuckRadioButton(
                     modifier = Modifier.padding(top = 8.dp, end = 12.dp),
                     text = label,
                     selected = selectedOption == option,
-                    onSelect = { selectedOption = option }
+                    onSelect = {
+                        selectedOption = option
+                        onValueChange(index)
+                               },
                 )
             }
         }
@@ -180,5 +238,26 @@ fun TradeWritePreview() {
 @Composable
 @Preview
 fun PhotoCountBoxPreview() {
-    PhotoCountBox()
+    PhotoCountBox(onImageClick = {})
 }
+
+fun handleTradeUpload(
+    tradeViewModel: TradeViewModel,
+    context: Context,
+    imageList: List<Uri>,
+    userId: String,
+    categoryId: String,
+    title: String,
+    detailDescription: String,
+    tradeLocation: String
+) {
+    val userIdBody = userId.toRequestBody("text/plain".toMediaTypeOrNull())
+    val categoryIdBody = categoryId.toRequestBody("text/plain".toMediaTypeOrNull())
+    val titleBody = title.toRequestBody("text/plain".toMediaTypeOrNull())
+    val contentBody = detailDescription.toRequestBody("text/plain".toMediaTypeOrNull())
+    val locationBody = tradeLocation.toRequestBody("text/plain".toMediaTypeOrNull())
+    val filePart = imageList[0].uriToMultipartBody(context)
+
+    tradeViewModel.uploadTrade(userIdBody, categoryIdBody, titleBody, contentBody, locationBody, filePart)
+}
+
