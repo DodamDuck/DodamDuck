6b2d652b622924a4600ef817b7a327374db9d83f
casio2978 <casio2978@naver.com>
2023-11-20 22:51:29 +0900

    [AOS] chore: 함수 네이밍 변경

--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/PostDetail.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/PostDetail.kt
@@ -9,26 +9,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
-import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -45,7 +35,7 @@ import androidx.navigation.compose.rememberNavController
 import org.chosun.dodamduck.R
 import org.chosun.dodamduck.model.repository.DummyItemFactory
 import org.chosun.dodamduck.ui.component.CommentIcon
-import org.chosun.dodamduck.ui.component.DodamDuckMessageInputFeild
+import org.chosun.dodamduck.ui.component.DodamDuckMessageInputField
 import org.chosun.dodamduck.ui.component.DodamDuckText
 import org.chosun.dodamduck.ui.component.SpannableText
 import org.chosun.dodamduck.ui.component.lazy_components.PostType
@@ -111,7 +101,7 @@ fun PostDetailScreen(navController: NavController) {
             PostDetailComments()
             Spacer(Modifier.weight(1f))
             Divider()
-            DodamDuckMessageInputFeild()
+            DodamDuckMessageInputField()
         }
     }
 }
