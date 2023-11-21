8efe7dbda93c01e870a545afda9ca22eca46ca99
casio2978 <casio2978@naver.com>
2023-11-21 21:32:06 +0900

    [AOS] feat: 채팅 목록 api 연동

---
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/model/dto/ChatListResponse.kt
@@ -0,0 +1,18 @@
+package org.chosun.dodamduck.model.dto
+
+data class ChatListDTO(
+    val chat_id: String,
+    val user1_id: String,
+    val user2_id: String,
+    val last_message: String,
+    val last_message_timestamp: String,
+    val post_title: String,
+    val post_image_url: String,
+    val seller_profile_url: String,
+    val category: String
+)
+
+data class ChatListResponse(
+    val error: Boolean,
+    val chat_list: List<ChatListDTO>?
+)
--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/model/viewmodel/ChatViewModel.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/model/viewmodel/ChatViewModel.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import org.chosun.dodamduck.model.dto.ChatInfo
+import org.chosun.dodamduck.model.dto.ChatListDTO
 import org.chosun.dodamduck.model.repository.ChatRepository
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
@@ -17,17 +18,26 @@ class ChatViewModel @Inject constructor(
     private val repository: ChatRepository
 ) : ViewModel() {
 
-    private val _chatLists = MutableStateFlow<List<ChatInfo>?>(null)
-    val chatLists: StateFlow<List<ChatInfo>?> = _chatLists
+    private val _chats = MutableStateFlow<List<ChatInfo>?>(null)
+    val chats: StateFlow<List<ChatInfo>?> = _chats
+
+    private val _chatList = MutableStateFlow<List<ChatListDTO>?>(null)
+    val chatList: StateFlow<List<ChatListDTO>?> = _chatList
 
     private val scheduledExecutorService = Executors.newSingleThreadScheduledExecutor()
 
-    suspend fun getChatLists(user1: String, user2: String) {
+    suspend fun getChats(user1: String, user2: String) {
         scheduledExecutorService.scheduleAtFixedRate({
             viewModelScope.launch {
-                _chatLists.value = repository.fetchChatList(user1, user2)
+                _chats.value = repository.fetchChats(user1, user2)
             }
         }, 0, 1, TimeUnit.SECONDS)
+    }
+
+    suspend fun getChatList(userId: String) {
+        viewModelScope.launch {
+            _chatList.value = repository.fetchChatList(userId)
+        }
     }
 
     fun sendChat(
