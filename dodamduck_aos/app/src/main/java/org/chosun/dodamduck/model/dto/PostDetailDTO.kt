fd850b5739d6f9796f58da6ea6112508b718fb51
casio2978 <casio2978@naver.com>
2023-11-25 15:50:39 +0900

    [AOS] feat: 글 상세 보기 DTO 클래스 선언

---
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/model/dto/PostDetailDTO.kt
@@ -0,0 +1,15 @@
+package org.chosun.dodamduck.model.dto
+
+data class PostDetailDTO(
+    val title: String,
+    val content: String,
+    val image_url: String,
+    val created_at: String,
+    val location: String,
+    val views: String,
+    val userName: String,
+    val userID: String,
+    val verification_count: String,
+    val categoryName: String,
+    val profile_url: String
+)
