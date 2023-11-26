602aefc5e0b86f18caa8efba6117faa072a88bc2
casio2978 <casio2978@naver.com>
2023-11-27 01:47:50 +0900

    [AOS] chore: Fake 객체 생성 함수 구현

--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/model/repository/DummyItemFactory.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/model/repository/DummyItemFactory.kt
@@ -3,10 +3,12 @@ package org.chosun.dodamduck.model.repository
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import org.chosun.dodamduck.R
+import org.chosun.dodamduck.model.dto.PostCommentDTO
+import org.chosun.dodamduck.model.dto.PostDetailDTO
 import org.chosun.dodamduck.model.state.PostDetailUserInfoState
 
 object DummyItemFactory {
-    
+
     @Composable
     fun createPostDetailUserInfoDummyList(): List<PostDetailUserInfoState> {
         return listOf(
@@ -16,7 +18,7 @@ object DummyItemFactory {
                 info = "1일전",
                 content = "요새 걱정이 너무 많네요.. 도와주세요"
             ),
-            
+
             PostDetailUserInfoState(
                 userName = "유리 맘",
                 userProfile = painterResource(id = R.drawable.img_user_profile_2),
@@ -25,4 +27,31 @@ object DummyItemFactory {
             )
         )
     }
+
+    fun createPostDetailDto() = PostDetailDTO(
+        "다양한 보드게임 원합니다.",
+        "가족과 함께 즐길 수 있는 다양한 보드게임, 교환 원합니다.",
+        "http://sy2978.dothome.co.kr/uploads/post_id18.jpg",
+        "2023-11-12 17:49:15",
+        "인천",
+        "21",
+        "홍길동",
+        "user1",
+        "5",
+        "교환",
+        "http://sy2978.dothome.co.kr/uploads/post_id18.jpg"
+    )
+
+    fun createPostComment() = PostCommentDTO(
+        "1",
+        "1",
+        "user1",
+        "저랑 교환 하실래요?",
+        "2023-11-12 17:49:15",
+        "2023-11-12 17:49:15",
+        "김철수",
+        "2",
+        "광주 동명동",
+        ""
+    )
 }
