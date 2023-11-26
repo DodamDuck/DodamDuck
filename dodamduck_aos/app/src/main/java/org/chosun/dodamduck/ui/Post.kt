86f06b51ef4a3303205461791f3d24a67013617e
casio2978 <casio2978@naver.com>
2023-11-26 16:02:49 +0900

    [AOS] feat: 게시글이 없을 때 보여줄 이미지 추가

--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/Post.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/Post.kt
@@ -1,6 +1,7 @@
 package org.chosun.dodamduck.ui
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -30,6 +31,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -94,6 +97,15 @@ fun PostScreen(
                     )
                     Divider()
                 }
+            }
+
+            if(posts.isEmpty()) {
+                Image(
+                    modifier = Modifier.fillMaxSize(),
+                    painter = painterResource(id = R.drawable.img_no_post),
+                    contentDescription = "No Post Image",
+                    contentScale = ContentScale.FillWidth
+                )
             }
         }
 
