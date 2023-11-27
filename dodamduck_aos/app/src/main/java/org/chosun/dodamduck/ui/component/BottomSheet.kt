20a2c299aef34ec20d2f12df63a35ef3309a9a5e
casio2978 <casio2978@naver.com>
2023-11-27 20:56:16 +0900

    [AOS] feat: 충돌 해결

--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/component/BottomSheet.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/ui/component/BottomSheet.kt
@@ -2,13 +2,16 @@ package org.chosun.dodamduck.ui.component
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.SheetState
 import androidx.compose.runtime.Composable
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BottomSheet(onDismissRequest: () -> Unit, content: @Composable () -> Unit) {
-    val sheetState = rememberModalBottomSheetState()
+fun BottomSheet(
+    sheetState: SheetState,
+    onDismissRequest: () -> Unit,
+    content: @Composable () -> Unit
+) {
     ModalBottomSheet(
         onDismissRequest = onDismissRequest,
         sheetState = sheetState
