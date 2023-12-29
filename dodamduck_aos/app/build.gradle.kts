879cc024b858774e99b72274bf893f1b66844524
casio2978 <casio2978@naver.com>
2023-12-29 18:49:30 +0900

    [AOS] chore: navigation test code 의존성 추가

--- dodamduck_aos/app/build.gradle.kts
+++ dodamduck_aos/app/build.gradle.kts
@@ -65,13 +65,15 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.compose.material:material:1.5.4")
-    implementation ("androidx.navigation:navigation-compose:2.7.4")
+    implementation ("androidx.navigation:navigation-compose:2.7.6")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-test-junit4:1.5.4")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.test.ext:junit-ktx:1.1.5")
+    implementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // hilt
     implementation("com.google.dagger:hilt-android:2.44")
@@ -97,11 +99,12 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.navigation:navigation-testing:2.7.4")
+    androidTestImplementation ("androidx.navigation:navigation-testing:2.7.6")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.5.4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
