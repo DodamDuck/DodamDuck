435ae6a489ab4f99adc2bb0c099818cbc4920f40
casio2978 <casio2978@naver.com>
2024-03-17 20:48:29 +0900

    [AOS] refactor: createChat UseCase 리팩토링

--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/domain/usecase/remote/post/CreateChat.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/domain/usecase/remote/post/CreateChat.kt
@@ -3,28 +3,19 @@ package org.chosun.dodamduck.domain.usecase.remote.post
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.collectLatest
+import org.chosun.dodamduck.data.dto.auth.AuthDto
+import org.chosun.dodamduck.data.dto.post.PostDto
 import org.chosun.dodamduck.domain.model.ApiResult
 import org.chosun.dodamduck.domain.repository.BasePostRepository
+import org.chosun.dodamduck.domain.usecase.remote.base.BaseResultUseCase
 import org.chosun.dodamduck.network.DodamDuckResponse
 import javax.inject.Inject
 
 class CreateChat<T> @Inject constructor(
     private val postRepo: BasePostRepository<T>
-) {
-    operator fun invoke(
-        postID: String,
-        userID: String
-    ): Flow<ApiResult<DodamDuckResponse>> = channelFlow {
-        postRepo.createChat(postID, userID).collectLatest { apiResult ->
-            if (apiResult is ApiResult.Success) {
-                send(ApiResult.Success(apiResult.value))
-            } else {
-                if (apiResult is ApiResult.Error) {
-                    send(apiResult)
-                } else if (apiResult is ApiResult.Exception) {
-                    send(apiResult)
-                }
-            }
-        }
+): BaseResultUseCase<PostDto, DodamDuckResponse>() {
+
+    override suspend fun execute(postDto: PostDto): Flow<ApiResult<DodamDuckResponse>> {
+        return postRepo.createChat(postDto.postID, postDto.userID)
     }
 }
