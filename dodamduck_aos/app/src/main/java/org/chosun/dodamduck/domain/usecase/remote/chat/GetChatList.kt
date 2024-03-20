98ced8675eeb1ef614b02cc5ce60240a0daef017
casio2978 <casio2978@naver.com>
2024-03-21 02:49:07 +0900

    [AOS] refactor: chat usecase 리팩토링

--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/domain/usecase/remote/chat/GetChatList.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/domain/usecase/remote/chat/GetChatList.kt
@@ -4,27 +4,19 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.collectLatest
 import org.chosun.dodamduck.data.dto.ChatListDTO
+import org.chosun.dodamduck.data.dto.ChatListResponse
+import org.chosun.dodamduck.data.dto.auth.AuthDto
 import org.chosun.dodamduck.domain.model.ApiResult
 import org.chosun.dodamduck.domain.repository.ChatRepository
+import org.chosun.dodamduck.domain.usecase.remote.base.BaseResultUseCase
+import org.chosun.dodamduck.network.DodamDuckResponse
 import javax.inject.Inject
 
 class GetChatList @Inject constructor(
     private val chatRepository: ChatRepository
-) {
+): BaseResultUseCase<String, ChatListResponse>(){
 
-    operator fun invoke(
-        userId: String
-    ): Flow<ApiResult<List<ChatListDTO>?>> = channelFlow {
-        chatRepository.fetchChatList(userId).collectLatest {  apiResult ->
-            if (apiResult is ApiResult.Success) {
-                send(ApiResult.Success(apiResult.value.chat_list))
-            } else {
-                if (apiResult is ApiResult.Error) {
-                    send(apiResult)
-                } else if (apiResult is ApiResult.Exception) {
-                    send(apiResult)
-                }
-            }
-        }
+    override suspend fun execute(userId: String): Flow<ApiResult<ChatListResponse>> {
+        return chatRepository.fetchChatList(userId)
     }
 }
--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/domain/usecase/remote/chat/GetChats.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/domain/usecase/remote/chat/GetChats.kt
@@ -6,25 +6,13 @@ import kotlinx.coroutines.flow.collectLatest
 import org.chosun.dodamduck.data.dto.ChatInfo
 import org.chosun.dodamduck.domain.model.ApiResult
 import org.chosun.dodamduck.domain.repository.ChatRepository
+import org.chosun.dodamduck.domain.usecase.remote.base.BaseResultUseCase
 import javax.inject.Inject
 
 class GetChats @Inject constructor(
     private val chatRepository: ChatRepository
-) {
-    operator fun invoke(
-        user1: String,
-        user2: String
-    ): Flow<ApiResult<List<ChatInfo>>> = channelFlow {
-        chatRepository.fetchChats(user1, user2).collectLatest { apiResult ->
-            if (apiResult is ApiResult.Success) {
-                send(ApiResult.Success(apiResult.value))
-            } else {
-                if (apiResult is ApiResult.Error) {
-                    send(apiResult)
-                } else if (apiResult is ApiResult.Exception) {
-                    send(apiResult)
-                }
-            }
-        }
+): BaseResultUseCase<Pair<String, String>, List<ChatInfo>>(){
+    override suspend fun execute(params: Pair<String, String>): Flow<ApiResult<List<ChatInfo>>> {
+        return chatRepository.fetchChats(params.first, params.second)
     }
 }
