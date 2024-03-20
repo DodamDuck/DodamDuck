f2b97bd51c3ec1f2eb316c867df20ce05593d583
casio2978 <casio2978@naver.com>
2024-03-21 01:29:14 +0900

    [AOS] feat: Back 버튼 두번 누를시 앱 종료

--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/presentation/trade/TradeSideEffect.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/presentation/trade/TradeSideEffect.kt
@@ -3,6 +3,9 @@ package org.chosun.dodamduck.presentation.trade
 import org.chosun.dodamduck.presentation.base.SideEffect
 
 sealed class TradeSideEffect: SideEffect {
+
+    data class Toast(val text: String): TradeSideEffect()
+
     object NavigatePopBackStack: TradeSideEffect()
 
     object NavigateToSearch: TradeSideEffect()
