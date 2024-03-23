ae7b01aeb1942fb94e874cd4cbec0d24374b1e95
casio2978 <casio2978@naver.com>
2024-03-24 00:31:30 +0900

    [AOS] refactor: ToyLibraryViewModel 비동기 결과 처리 리팩토링

--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/presentation/library/ToyLibraryReducer.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/presentation/library/ToyLibraryReducer.kt
@@ -5,9 +5,8 @@ import org.chosun.dodamduck.presentation.base.Reducer
 class ToyLibraryReducer(state: ToyLibraryState): Reducer<ToyLibraryState, ToyLibraryEvent>(state) {
     override fun reduce(oldState: ToyLibraryState, event: ToyLibraryEvent) {
         when(event) {
-            is ToyLibraryEvent.OnRequestData -> setState(oldState.copy(isLoading = true))
-            is ToyLibraryEvent.OnFetchToyList -> setState(oldState.copy(toyList = event.toyList))
-            is ToyLibraryEvent.OnCompleteData -> setState(oldState.copy(isLoading = false, isError = false))
+            is ToyLibraryEvent.OnLoading -> setState(oldState.copy(isLoading = true))
+            is ToyLibraryEvent.OnSuccessToyList -> setState(oldState.copy(isLoading = false, isError = false, toyList = event.toyList))
             is ToyLibraryEvent.OnError -> setState(oldState.copy(isLoading = false, isError = true))
         }
     }
