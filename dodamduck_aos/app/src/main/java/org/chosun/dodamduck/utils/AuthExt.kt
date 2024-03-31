8ec1d9f2ff879754b32fe7f6c618262bd20fd97b
casio2978 <casio2978@naver.com>
2024-03-31 18:23:57 +0900

    [AOS] fix: 아이디 검증 코드 수정

--- dodamduck_aos/app/src/main/java/org/chosun/dodamduck/utils/AuthExt.kt
+++ dodamduck_aos/app/src/main/java/org/chosun/dodamduck/utils/AuthExt.kt
@@ -2,5 +2,5 @@ package org.chosun.dodamduck.utils
 
 object AuthExt {
     fun String.verifyId(): Boolean
-        = this.matches(Regex("^(?=.*[a-z])[a-z](?=.*\\d)[a-zA-Z\\d]{7,15}$"))
+        = this.matches(Regex("[a-z](?=.*\\d)[a-zA-Z\\d]{7,15}$"))
 }
